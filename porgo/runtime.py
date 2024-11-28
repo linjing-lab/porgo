f2f92bcae60a3ee9b1e54cb5ba0c8c252151e368
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2024-11-28 22:32:44 +0800

    delete indication

--- porgo/runtime.py
+++ porgo/runtime.py
@@ -23,7 +23,7 @@ class glos:
         assert all(map(lambda x: isinstance(x, (List, Tuple)), bounds))
         if not isinstance(bounds, NUMPY_NDARRAY):
             self.bounds = numpy.array(bounds)
-        assert numpy.all(self.bounds[:,0] < self.bounds[:,1]), 'Search bounds use the form as [[-10, 10]] * 3 or [(-10, 10)] * 3.'
+        assert numpy.all(self.bounds[:,0] < self.bounds[:,1])
         self.F = mutation
         self.CR = recombination
 
